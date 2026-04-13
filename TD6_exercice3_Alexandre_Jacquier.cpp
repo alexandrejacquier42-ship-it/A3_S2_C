d4e87335d2ecd010b29177646c1de71147c97e01
Alexandre Jacquier <alexandre.jacquier42@gmail.com>
2026-04-13 09:31:52 +0200

    TD6 ex 3 V1

--- TD6_exercice3_Alexandre_Jacquier.cpp
+++ TD6_exercice3_Alexandre_Jacquier.cpp
@@ -1,65 +1,8 @@
+
 #include <iostream>
 #include<cmath>
 #include<string>
 using namespace std;
-
-class Point {
-    private :
-        int x, y;
-    
-    public : 
-        
-        Point(int valX, int valY) {
-            x = valX;
-            y = valY;
-        }
-        
-        double distance(Point &autre) {
-            double x_dif = autre.x - this->x;
-            double y_dif = autre.y - this->y;
-            return sqrt(pow(x_dif,2)+pow(y_dif, 2));
-        }
-
-        Point milieu(Point &autre) {
-            double milieu_x =  (this->x + autre.x)/2;
-            double milieu_y =  (this->y + autre.y)/2;
-            Point p_milieu(milieu_x, milieu_y);
-            return p_milieu;
-        }
-
-        virtual void afficher() {
-            cout <<"("<< x <<","<< y <<")"<< endl;
-        }        
-        int getX(){
-            return x;
-        }
-        int getY(){
-            return y;
-        }
-};
-
-class Rectangle {
-    private :
-        Point hautGauche();
-        int longueur, largeur;
-        static int nbRectangles;
-        Rectangle(Point &p1, Point &p2){
-            largeur=p1.getX-p2.getX;
-            longueur=p1.getY-p2.getY;
-        }
-    public :
-        static int incr_nbRectangle (){
-            nbRectangles ++;
-        }
-
-        static int decr_nbRectangle(){
-            nbRectangles --;
-        }
-        Rectangle creerDepuisPoints(Point &p1, Point &p2){
-            
-        }
-};
-
 
 class Client {
     private: 
@@ -127,16 +70,8 @@ class FileDAttente {
 
 }
 
-int main() {
-    Point P1(0,0);
-    Point P2(3,4);
-    double d = P1.distance(P2);
-    cout << "La distance entre P1 et P2 est : " << d << endl;
-    Point pM = P1.milieu(P2);
-    cout << "Le point milieu est : ";
-    pM.afficher();
-
-        FileDAttente file(2);
+int main(){
+    FileDAttente file(2);
     Client c1("Dupont", "Jean");
     Client c2("Durand", "Marie");
     Client c3("Martin", "Paul");
@@ -145,6 +80,31 @@ int main() {
     file.ajouter(c2);
     file.ajouter(c3);
     
+    cout << "Clients dans la file d'attente:" << endl;
+    for (int i = 0; i < 3; i++) {
+        file.getClient(i).afficher();
+    }
+
+    file.suivant(); // Remove the first client (c1)
+
+    cout << "Clients après avoir retiré le premier client:" << endl;
+    for (int i = 0; i < 2; i++) {
+        file.getClient(i).afficher();
+    }
+
+    return 0;
+}
+
+int main(){
+    FileDAttente file(2);
+    Client c1("Dupont", "Jean");
+    Client c2("Durand", "Marie");
+    Client c3("Martin", "Paul");
+    
+    file.ajouter(c1);
+    file.ajouter(c2);
+    file.ajouter(c3);
+
     cout << "Clients dans la file d'attente:" << endl;
     for (int i = 0; i < 3; i++) {
         file.getClient(i).afficher();
